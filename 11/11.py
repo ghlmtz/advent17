2a4d07007819cd91698da303954bce2aaa1f9700
ghlmtz <devilishgiant@gmail.com>
2017-12-11 00:17:19 -0500

    Day 11 done

---
+++ 11/11.py
@@ -0,0 +1,29 @@
+dirs = open('11.in','r').read().split(',')
+# open('11.in'),'r').read()
+#dirs = ['se','sw','se','sw','sw','ne','ne']
+axis = [0,0,0]
+maxdist = 0
+for d in dirs:
+	if d == 'sw':
+		axis[2] += 1
+		axis[0] -= 1
+	if d == 's':
+		axis[2] += 1
+		axis[1] -= 1
+	if d == 'n':
+		axis[1] += 1
+		axis[2] -= 1
+	if d == 'ne':
+		axis[0] += 1
+		axis[2] -= 1
+	if d == 'nw':
+		axis[1] += 1
+		axis[0] -= 1
+	if d == 'se':
+		axis[0] += 1
+		axis[1] -= 1
+	m = max(map(abs, axis))
+	if m > maxdist:
+		maxdist = m
+print(m)
+print(maxdist)
